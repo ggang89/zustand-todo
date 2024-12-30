e447972da6aa6e380fcedf0d63ffed16c51c63f2
ggang89 <ggang89@gmail.com>
2024-12-30 14:37:34 +0900

    input창도 zustand로 했을 경우

--- src/components/todo-input.tsx
+++ src/components/todo-input.tsx
@@ -1,31 +1,33 @@
-//import { useTodoInputStore } from "../stores/input-store";
-import { useState } from "react";
+import { useTodoInputStore } from "../stores/input-store";
+//import { useState } from "react";
 import { useTodoListStore } from "../stores/todo-list-store";
 
 const TodoInput = () => {
-  // const todo = useTodoInputStore((state) => state.todo);
-  // const handleInput = useTodoInputStore((state) => state.handleInput);
+  const todo = useTodoInputStore((state) => state.todo);
+  const handleInput = useTodoInputStore((state) => state.handleInput);
 
-  const [input, setInput] = useState("");
+  //const [input, setInput] = useState("");
   const addTodoBtn = useTodoListStore((state) => state.addTodoBtn);
 
   
   const addNewTodo = (todo: string) => {
     addTodoBtn(todo);
-    setInput("");
+    //setInput("");
+    handleInput("")
     console.log(todo)
   };
 
+ 
   return (
     <div>
       <label htmlFor="newTodo">New Todo </label>
       <input
         id="newTodo"
-        value={input}
+        value={todo}
         type="text"
-        onChange={(e) => setInput(e.target.value)}
+        onChange={(e) => handleInput(e.target.value)}
       />
-      <button onClick={() => addNewTodo(input)}>➕ 추가</button>
+      <button onClick={() => addNewTodo(todo)}>➕ 추가</button>
     </div>
   );
 };
