6f4463c203d169198937af9b1722ff674286f3cb
ggang89 <ggang89@gmail.com>
2025-01-06 14:39:28 +0900

    문자열 눌렀을 때도 text 편집할 수 있도록 기능 추가 , css 수정

--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -3,16 +3,17 @@ import { useTodoListStore } from "../stores/todo-list-store";
 const TodoList = () => {
   const { todoList, removeTodo, editBtn, handleEditTodo, doneCheck } =
     useTodoListStore();
-  const enter = (e: React.KeyboardEvent<HTMLInputElement>,id:string) => {
+  const enter = (e: React.KeyboardEvent<HTMLInputElement>, id: string) => {
     if (e.key === "Enter") {
-     editBtn(id); }
+      editBtn(id);
+    }
   };
   return (
     <>
       {todoList.map((todo) => (
         <li key={todo.id} className="px-3">
           {todo.isEditing ? (
-            <div className="flex gap-1">
+            <div className="flex gap-1 items-center border-b-2 ">
               <input
                 value={todo.todoTitle}
                 type="text"
@@ -22,7 +23,7 @@ const TodoList = () => {
                 onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) =>
                   enter(e, todo.id)
                 }
-                className="flex-1 px-3 rounded-md focus:outline-none focus:ring-2 focus: ring-red-400 "
+                className="h-[38px] flex-1 px-3 my-3 rounded-md focus:outline-none focus:ring-2 focus: ring-red-400 font-bold italic text-gray-600"
               />
               <button
                 className="h-[35px] text-sm border-0 rounded-md bg-slate-500 text-white font-bold px-2 py-1 hover:bg-gray-900"
@@ -36,11 +37,14 @@ const TodoList = () => {
             <div className="flex gap-1 border-b-2 border-gray-200 py-3 items-center justify-center ">
               <input type="checkbox" onClick={() => doneCheck(todo.id)} />
               {todo.isDone ? (
-                <p className="line-through font-bold text-base text-slate-600 flex-1 italic px-2">
+                <p className=" line-through font-bold text-base text-slate-600 flex-1 italic px-2">
                   {todo.todoTitle}
                 </p>
               ) : (
-                <p className="font-bold text-base flex-1 px-2">
+                <p
+                  className="font-bold text-base flex-1 px-2 cursor-pointer"
+                  onClick={() => editBtn(todo.id)}
+                >
                   {todo.todoTitle}
                 </p>
               )}
